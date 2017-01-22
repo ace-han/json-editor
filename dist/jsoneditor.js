c221c8d3f64fe6ef8de09c208832565d2f5df254
Ace <ace.jl.han@gmail.com>
2017-01-23 00:58:25 +0800

    Make jsoneditor conditionally as CommonJS module

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -8042,7 +8042,16 @@ JSONEditor.defaults.resolvers.unshift(function(schema) {
   }
 })();
 
-  window.JSONEditor = JSONEditor;
+
+  // Make conditionally as CommonJS module
+  // please refer to
+  // https://github.com/brettz9/json-editor/blob/0ebbd538925ebb82247525657653562cfcdadaf3/src/outro.js
+  if (typeof module !== 'undefined') {
+     module.exports = JSONEditor;
+  }
+  else {
+     window.JSONEditor = JSONEditor;
+  }
 })();
 
 //# sourceMappingURL=jsoneditor.js.map
--- src/outro.js
+++ src/outro.js
@@ -1,2 +1,11 @@
-  window.JSONEditor = JSONEditor;
+
+  // Make conditionally as CommonJS module
+  // please refer to
+  // https://github.com/brettz9/json-editor/blob/0ebbd538925ebb82247525657653562cfcdadaf3/src/outro.js
+  if (typeof module !== 'undefined') {
+     module.exports = JSONEditor;
+  }
+  else {
+     window.JSONEditor = JSONEditor;
+  }
 })();
